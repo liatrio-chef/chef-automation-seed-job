8cbeec61cab19d2cfd48801ba06458f622945d52
Drew Holt <drew@liatrio.com>
2016-12-02 14:59:10 -0700

    LTA-42 check that berkshef is updated before chefspec as changes in metadata will fail unit tests

--- unit_test.sh
+++ unit_test.sh
@@ -3,6 +3,9 @@ chef --version
 rubocop --version
 foodcritic --version
 
+echo "Updating Berkshelf: "
+if [ ! -f Berksfile.lock ]; then berks install; else berks update; fi;
+
 echo "Starting chefstyle (rubocop): "
 rubocop --color
 
